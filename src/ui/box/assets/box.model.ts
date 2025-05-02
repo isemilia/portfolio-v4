89858fa32f6b371be2ff3fa32d66183010f91612
Emilia <81705094+isemilia@users.noreply.github.com>
2025-05-02 14:28:54 +0500

    Type theme

--- src/ui/box/assets/box.model.ts
+++ src/ui/box/assets/box.model.ts
@@ -1,7 +1,8 @@
 import { CSSObject, DefaultTheme } from 'styled-components';
 import { ComponentType } from 'react';
+import palette from '../../../shared/theme/palette.ts';
 
 export interface TBoxProps {
-  sx?: (theme: DefaultTheme) => CSSObject;
+  sx?: (theme: DefaultTheme & { palette: typeof palette }) => CSSObject;
   as?: keyof HTMLElementTagNameMap | ComponentType<any>;
 }
